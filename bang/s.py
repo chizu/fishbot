df17393f2e3147c3c200e440f58ead6e7fbbde69
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-02-26 02:04:20 +0000

    Fixed IndexError when !s was run with improper arguments.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@4 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- bang/s.py
+++ bang/s.py
@@ -11,9 +11,10 @@ def getnick(s):
 def handle_say(self, source, to, message):
     respond = self.respond_to(source, to)
     try:
-	pattern = message.split('/')[1]
-	repl = message.split('/')[2]
-	self.say(respond, "%s meant to say: %s" % (getnick(source), re.sub(pattern, repl, self.last[getnick(source)][0])))
+	if len(message.split('/')) == 2:
+	    pattern = message.split('/')[1]
+	    repl = message.split('/')[2]
+	    self.say(respond, "%s meant to say: %s" % (getnick(source), re.sub(pattern, repl, self.last[getnick(source)][0])))
     except:
 	self.say(respond, "Invalid regular expression.")
 	raise
