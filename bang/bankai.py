d0e7c563297af517d7d19f77f1f9be820d0a8836
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-03-14 22:33:19 +0000

    Fixed grammar mistake.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@15 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- bang/bankai.py
+++ bang/bankai.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 def handle_say(self, source, to, message):
     respond = self.respond_to(source, to)
-    self.action(respond, "begins begins to glow with power, and gains 20 strength and 10 will.")
+    self.action(respond, "begins to glow with power, and gains 20 strength and 10 will.")
