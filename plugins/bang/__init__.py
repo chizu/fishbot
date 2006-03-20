f6f3211fed3f89825da7d9bd267c958c3d35dd6d
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-03-20 00:31:15 +0000

    Simple anti-spam.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@29 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/__init__.py
+++ plugins/bang/__init__.py
@@ -9,6 +9,9 @@ for each in submodules:
 __all__.remove('__init__')
 
 def bang(self, event):
+    # Early exit for spam.
+    if self.last[self.getnick(event.source())][0] == event.arguments()[0]:
+        return
     import re
     import importer
     match = re.search(expression[0], event.arguments()[0])
