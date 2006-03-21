2b549bbd551a9dcfe60db5f05d73a709225cf496
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-03-21 04:27:44 +0000

    Fortune dictionary argument.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@30 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/fortune.py
+++ plugins/bang/fortune.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
-import os
+import os,re
 
 def handle_say(self, source, to, message):
     respond = self.respond_to(source, to)
-    for each in os.popen('fortune').readlines():
+    if len(message.split(' ')) >= 2:
+        fortune_dictionary = message.split(' ')[1]
+    else:
+        fortune_dictionary = ""
+    for each in os.popen('fortune %s' % re.escape(fortune_dictionary)).readlines():
 	self.say(respond, each)
