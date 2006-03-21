527f740244017def9b2165805ea89db8bc6f695c
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-03-21 04:31:54 +0000

    Fortune size limited.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@31 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/fortune.py
+++ plugins/bang/fortune.py
@@ -7,5 +7,5 @@ def handle_say(self, source, to, message):
         fortune_dictionary = message.split(' ')[1]
     else:
         fortune_dictionary = ""
-    for each in os.popen('fortune %s' % re.escape(fortune_dictionary)).readlines():
+    for each in os.popen('fortune -n 500 -s %s' % re.escape(fortune_dictionary)).readlines():
 	self.say(respond, each)
