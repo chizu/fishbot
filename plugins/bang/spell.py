471625c1aff4d0408941c9c4270cdb71b8dc18b6
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-04-06 23:55:56 +0000

    Spelling.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@44 06b493c4-6b0d-0410-8f1d-df2a14a3f893

---
+++ plugins/bang/spell.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os,re
+
+def handle_say(self, source, to, message):
+    respond = self.respond_to(source, to)
+    if len(message.split(' ')) >= 2:
+        spelling = message.split(' ')[1:]
+    else:
+        spelling = [self.last[self.getnick(source)][0]]
+    for spell in spelling:
+        for each in os.popen('echo %s | aspell -a' % re.escape(spell)).readlines():
+            for each in each.split("\n"):
+                if len(each) > 0 and each[0] == '&':
+                    self.say(respond, each)
