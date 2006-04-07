52484ce58c3ac626df3ff2102a8644ce4a761365
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-04-07 00:38:41 +0000

    Plugins executed in alphanumeric order, to deal with plugin dependencies.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@48 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,11 +7,15 @@ from glob import glob
 expressions = {}
 
 submodules = glob("plugins/*")
+submodules.sort()
 __all__ = set()
 for each in submodules:
     each = each.split('/')[-1].split('.')[0]
     __all__.add(each)
 __all__.remove('__init__')
+__all__ = list(__all__)
+__all__.sort()
+print __all__
 
 for each in __all__:
     module = importer.__import__(name=each, path="plugins")
