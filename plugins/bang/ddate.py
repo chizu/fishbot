5b6e0bdd78d0274d501611dd3dcf44b16361b1c5
pountaar <pountaar@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-10-07 18:26:50 +0000

    git-svn-id: https://fishbot.spicious.com/svn/fishbot@78 06b493c4-6b0d-0410-8f1d-df2a14a3f893

---
+++ plugins/bang/ddate.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""!ddate - Print out the discordian date..
+Usage: !ddate [+format]"""
+import os,re
+
+def bang(pipein, arguments, event):
+    if len(arguments) > 0:
+        options = arguments
+    else:
+        options = ""
+    return (os.popen('ddate %s' % re.escap(options)).readlines(), None)
