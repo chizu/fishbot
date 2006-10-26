8f08775af5178d767d2a4ec8d7d5638dd66c1db5
lotyrin <lotyrin@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-10-26 20:38:13 +0000

    Removed my experimental feature.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@87 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/snackstats.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-"""Cookies are delicious delicacies."""
-import fishapi
-def bang(pipein, arguments, event):
-    return (str (fishapi.backend.sql_query("select count(*) as count, split_part( source, '!', 1) as nick FROM events WHERE arguments ~'!botsnack' GROUP BY nick;")[0][0], None))
