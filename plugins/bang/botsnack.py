ad1d1378e2a6f008c04920f48a84cd037cd480c1
lotyrin <lotyrin@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-10-26 20:11:59 +0000

    Removed what seems to be an obsolete condition from !botsnack.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@83 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/botsnack.py
+++ plugins/bang/botsnack.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
 """Cookies are delicious delicacies."""
 def bang(pipein, arguments, event):
-    if event.source()[0:6] == "Sauce!":
-        return
-    else:
-        return (":)", None)
+    return (":)", None)
