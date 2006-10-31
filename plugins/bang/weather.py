3d47ce3d912e04ef2801d52d368b5cd778a5cad5
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-10-31 01:35:48 +0000

    Weather testing.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@96 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/weather.py
+++ plugins/bang/weather.py
@@ -5,5 +5,5 @@ import urllib, re, fishapi
 def bang(pipein, arguments, event):
     location = pipein or arguments
     url = "http://rss.wunderground.com/auto/rss_full/%s.xml?units=both" % location
-    groups = fishapi.http_grep(url, "<title>(.*?) Weather from Weather Underground</title>.*<\!\[CDATA\[(.*?)\]\]>")
-    return (groups[0] + ' - ' + groups[1], None)
+    groups = fishapi.http_grep(url, "<\!\[CDATA\[(.*?)\]\]>")
+    return (location + ' - ' + groups[0], None)
