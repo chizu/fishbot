eb72598e03316ca9eed34fe476b1d6df28c3e78c
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-10-31 01:37:04 +0000

    Unquote weather.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@97 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/weather.py
+++ plugins/bang/weather.py
@@ -6,4 +6,4 @@ def bang(pipein, arguments, event):
     location = pipein or arguments
     url = "http://rss.wunderground.com/auto/rss_full/%s.xml?units=both" % location
     groups = fishapi.http_grep(url, "<\!\[CDATA\[(.*?)\]\]>")
-    return (location + ' - ' + groups[0], None)
+    return (location + ' - ' + urllib.unquote(groups[0]), None)
