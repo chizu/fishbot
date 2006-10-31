c28d422fa39205596524a886cc4c0c8b53d0862e
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-10-31 01:52:41 +0000

    Unquote doesn't work here.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@98 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/weather.py
+++ plugins/bang/weather.py
@@ -6,4 +6,4 @@ def bang(pipein, arguments, event):
     location = pipein or arguments
     url = "http://rss.wunderground.com/auto/rss_full/%s.xml?units=both" % location
     groups = fishapi.http_grep(url, "<\!\[CDATA\[(.*?)\]\]>")
-    return (location + ' - ' + urllib.unquote(groups[0]), None)
+    return (location + ' - ' + re.sub('&#176;', chr(176), groups[0]), None)
