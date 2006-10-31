535814842f9e5fd103661caed3412697b621ce07
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-10-31 00:45:06 +0000

    + != %
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@92 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/slogan.py
+++ plugins/bang/slogan.py
@@ -3,6 +3,6 @@ import fishapi,re
 
 def bang(pipein, arguments, event):
     subject = pipein or arguments
-    url = "http://www.sloganizer.net/en/?slogan=%s" + subject
+    url = "http://www.sloganizer.net/en/?slogan=%s" % subject
     groups = fishapi.http_grep(url, '<p class="slogan">.<b>(.*)</b>.</p>')
     return (groups[0], None)
