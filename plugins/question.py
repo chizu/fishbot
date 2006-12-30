b7d040d08d89cebd9ff5778a69193b4f5b0d0350
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2006-12-30 01:54:28 +0000

    Bug fix for questions plugin.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@101 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/question.py
+++ plugins/question.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python
 def questions(self, event):
     import random
-    responses = [
-        # Standard eight ball
-        "Signs point to yes.",
-        "Yes.",
-        "Most likely.",
-        "Without a doubt.",
-        "Yes - definitely.",
-        "As I see it, yes.",
-        "You may rely on it.",
-        "Outlook good.",
-        "It is certain.",
-        "It is decidedly so.",
-        "Reply hazy, try again.",
-        "Better not tell you now.",
-        "Ask again later.",
-        "Concentrate and ask again.",
-        "Cannot predict now.", 
-        "My sources say no.",
-        "Very doubtful.",
-        "My reply is no.",
-        "Outlook not so good.",
-        "Don't count on it.",
-        # Affirmation ball
-        "You look marvelous.",
-        "Your breath is so minty.",
-        # Sarcastic Ball
-        "Do I Look Like I Care?",
-        "Yeah, Right.",
-        # Random
-        "The sky might be octarine. I would go check."
-        ]
-    self.say(self.respond_to(event.source(), event.target()), random.choice(responses))
-
-expression = (self.connection.get_nickname() + '.*?\?\b*$', questions)
+    if self.connection.get_nickname() in event.arguments()[0]:
+        responses = [
+            # Standard eight ball
+            "Signs point to yes.",
+            "Yes.",
+            "Most likely.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "As I see it, yes.",
+            "You may rely on it.",
+            "Outlook good.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Reply hazy, try again.",
+            "Better not tell you now.",
+            "Ask again later.",
+            "Concentrate and ask again.",
+            "Cannot predict now.", 
+            "My sources say no.",
+            "Very doubtful.",
+            "My reply is no.",
+            "Outlook not so good.",
+            "Don't count on it.",
+            # Affirmation ball
+            "You look marvelous.",
+            "Your breath is so minty.",
+            # Sarcastic Ball
+            "Do I Look Like I Care?",
+            "Yeah, Right.",
+            # Random
+            "The sky might be octarine. I would go check."
+            ]
+        self.say(self.respond_to(event.source(), event.target()), random.choice(responses))
+        
+expression = ('.*\?\b*$', questions)
