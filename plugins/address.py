4e0cddc64cc6c1174b7ece180d9a1ded24b14e26
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-04 08:06:30 +0000

    Working pluging for addressing commands directly to Fishbot.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@106 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/address.py
+++ plugins/address.py
@@ -2,9 +2,12 @@
 """Execute !<command> code when '<nickname> <command>' is said."""
 def address(self, event):
     import re, plugins.bang
-    prefix = "^\b" + self.connection.get_nickname()
+    prefix = "^\s*" + self.connection.get_nickname() + "\W+"
+    print prefix
     if re.search(prefix, event.arguments()[0]):
-        event.arguments()[0] = re.sub(prefix, event.arguments()[0], "!")
-        bang.bang(self, event)
+        print event.arguments()[0]
+        event.arguments()[0] = re.sub(prefix, "!", event.arguments()[0])
+        print event.arguments()[0]
+        plugins.bang.bang(self, event)
 
-expression = ('.*\?\b*$', address)
+expression = ('.*', address)
