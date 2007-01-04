ffca1cbe7fb5f7ff4f9c1641f7a1beedd6e30718
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-04 08:07:11 +0000

    address.py debugging statements removed.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@107 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/address.py
+++ plugins/address.py
@@ -3,11 +3,8 @@
 def address(self, event):
     import re, plugins.bang
     prefix = "^\s*" + self.connection.get_nickname() + "\W+"
-    print prefix
     if re.search(prefix, event.arguments()[0]):
-        print event.arguments()[0]
         event.arguments()[0] = re.sub(prefix, "!", event.arguments()[0])
-        print event.arguments()[0]
         plugins.bang.bang(self, event)
 
 expression = ('.*', address)
