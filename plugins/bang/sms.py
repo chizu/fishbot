6716e1f6e628d3e91da1e5a390db3868aa7e2503
pountaar <pountaar@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-11 04:23:30 +0000

    Added check functionality, still working on callbacks and timers.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@116 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/sms.py
+++ plugins/bang/sms.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 """An SMS module for fishbot.  This mostly acts as a mail relay."""
+
 def bang(pipein, arguments, event):
-
     import imaplib
-
+    from threading import Timer
     server = 'spicious.com' #options for the script: mail server
     port = 993 # port (this is for SSL)
     user = 'irc'
     passwd = 'SMS2ircw00t'
-
-    arguments = arguments.split()
 
     def login():
         mc = imaplib.IMAP4_SSL(server, port)
         mc.login(user,passwd)
         mc.select()
         return mc
+
+    arguments = arguments.split()
 
     if len(arguments) < 1:
         return("Please pass a command to !sms: list")
@@ -36,12 +36,12 @@ def bang(pipein, arguments, event):
 
     if arguments[0] == 'read' and len(arguments) == 2:
         outstr = []
-        messageno = arguments[1]
+        mno = arguments[1]
         mc = login()
-        typ, data = mc.fetch(messageno, '(BODY[HEADER.FIELDS (SUBJECT FROM)] BODY[TEXT])')
+        typ, data = mc.fetch(mno, '(BODY[HEADER.FIELDS (SUBJECT FROM)] BODY[TEXT])')
         if typ == 'NO':
             return("Error fetching message", None)
-        outstr.append("Message " + messageno)
+        outstr.append("Message " + mno)
         headers = data[0][1].split("\r\n")
         outstr.append(headers[0])
         outstr.append(headers[1])
@@ -53,15 +53,33 @@ def bang(pipein, arguments, event):
         del(mc)
         return(outstr, None)
 
-    if arguments[0] == 'delete' and len(arguments) == 2:
+    if arguments[0] == 'del' and len(arguments) == 2:
         outstr = []
-        messageno = arguments[1]
+        mno = arguments[1]
         mc = login()
-        typ, data = mc.store(messageno, '+FLAGS', '\\Deleted')
+        typ, data = mc.store(mno, '+FLAGS', '\\Deleted')
         if typ == 'OK':
-            outstr.append("Message " + messageno + " successfully deleted.")
+            outstr.append("Message " + mno + " successfully deleted.")
         mc.expunge()
         mc.close()
         mc.logout()
         del(mc)
+        return(outstr, None)
+
+    if arguments[0] == 'check':
+        outstr = []
+        mc = login()
+        typ, newmessages = mc.search(None, 'NEW')
+        newmessages = newmessages[0].split()
+        if len(newmessages) > 0:
+            outstr.append("New SMS Arrived:")
+        for mno in newmessages:
+            typ, data = mc.fetch(mno, '(BODY[HEADER.FIELDS (SUBJECT FROM)] BODY[TEXT])')
+            headers = data[0][1].split("\r\n")
+            outstr.append(headers[0])
+            outstr.append(headers[1])
+            body = data[1][1].split("\r\n")
+            body = data[1][1].split("\r\n")
+            for line in body:
+                outstr.append(line)
         return(outstr, None)
