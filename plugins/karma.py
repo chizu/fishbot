323801ebc265ca894b58399773c664a46ad9e1f0
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-16 06:13:14 +0000

    Fishbot karma powers.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@141 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/karma.py
+++ plugins/karma.py
@@ -11,6 +11,13 @@ def karma(self, event):
     import re, fishapi
     before, string, operator = re.search(expression[0], event.arguments()[0]).groups()
     string = string.strip().lower()
+    count = 0
+    for each in backend.last(event.source(),25):
+        if string + operator in each[4]:
+            count++
+    if count > 5:
+        Karma(string=fishapi.getnick(event.source())).score -= 50
+        return
     if operator == '++':
         Karma(string=string).score += 1
     elif operator == '--':
