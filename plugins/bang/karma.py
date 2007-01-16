1727cf8513c9cae7d12e2f0dda25bc0d3dc991c5
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-16 06:33:28 +0000

    Another karma fix.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@145 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/karma.py
+++ plugins/bang/karma.py
@@ -9,7 +9,7 @@ def bang(pipein, arguments, event):
         all_karma = [(x.score, x.string) for x in Karma(-1)]
         large = max(all_karma)
         small = min(all_karma)
-        return "Highest score: %s (%s) - Lowest score: %s (%s)" % (large.string, large.score, small.string, small.score)
+        return "Highest score: %s (%s) - Lowest score: %s (%s)" % (large[0], large[1], small[0], small[1])
     else:
         thing = Karma(-1, string=arguments)
         if thing:
