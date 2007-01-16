00ccc2e5e35349e95b1a3d4e6d3e9f3a2607dff9
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-16 06:31:10 +0000

    Karma stats.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@143 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/karma.py
+++ plugins/bang/karma.py
@@ -5,6 +5,12 @@ import backend
 from plugins.karma import Karma
 
 def bang(pipein, arguments, event):
-    thing = Karma(-1, string=arguments)
-    if thing:
-        return ("'%s' has a score of: %s" % (arguments, thing.score), None)
+    if arguments.strip() == 'stats':
+        all_karma = [x.score, x.string for x in Karma(-1)]
+        large = max(all_karma)
+        small = min(all_karma)
+        return "Highest score: %s (%s) - Lowest score: %s (%s)" % (large.string, large.score, small.string, small.score)
+    else:
+        thing = Karma(-1, string=arguments)
+        if thing:
+            return ("'%s' has a score of: %s" % (arguments, thing.score), None)
