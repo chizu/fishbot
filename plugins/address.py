f32e5571ba7bbda96f8cd41b9db33218086045c8
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-23 09:07:19 +0000

    Deep copy needed for event objects.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@159 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/address.py
+++ plugins/address.py
@@ -4,7 +4,7 @@ def address(self, event):
     import re, bang, copy
     prefix = "^\s*" + self.connection.get_nickname() + "\W+"
     if len(event.arguments()) > 0 and re.search(prefix, event.arguments()[0]):
-        bang_event = copy.copy(event)
+        bang_event = copy.deepcopy(event)
         bang_event.arguments()[0] = re.sub(prefix, "!", bang_event.arguments()[0])
         bang.bang(self, bang_event)
     
