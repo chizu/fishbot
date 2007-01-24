c870d1b0a2e9bc9696cbdec3e976d0a42da19eb3
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-24 09:00:10 +0000

    Preserve the case and spacing for bang.karma arguments.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@165 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/karma.py
+++ plugins/bang/karma.py
@@ -5,16 +5,16 @@ import backend, fishapi
 from plugins.karma import Karma
 
 def bang(pipein, arguments, event):
-    arguments = arguments.strip().lower()
-    if not arguments:
-        arguments = fishapi.getnick(event.source()).strip().lower()
-    if arguments == 'stats':
+    token = arguments.strip().lower()
+    if not token:
+        token = fishapi.getnick(event.source()).strip().lower()
+    if token == 'stats':
         all_karma = [(x.score, x.string) for x in Karma(-1)]
         large = max(all_karma)
         small = min(all_karma)
         return ("Highest score: %s (%s) - Lowest score: %s (%s)" % (large[0], large[1], small[0], small[1]), None)
     else:
-        thing = Karma(-1, string=arguments)
+        thing = Karma(-1, string=token)
         if thing:
             return ("'%s' has a score of: %s" % (arguments, thing.score), None)
         else:
