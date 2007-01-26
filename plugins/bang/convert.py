f1ffe03c940f8a1e983a7a7f4bb5987925087014
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-01-26 09:27:41 +0000

    Remove old implementation of bang.convert.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@167 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/convert.py
+++ plugins/bang/convert.py
@@ -17,30 +17,3 @@ def bang(pipein, arguments, event):
         return (response, None)
     except NameError:
         return ("Look it up yourself, Google sucks.", None)
-
-def handle_say(self, source, to, message):
-    # self here is going to be the main Fishbot object
-    respond = self.respond_to(source, to)
-    if (re.search("^!convert (.*)", message)):
-	# probably shouldn't be considered a simple command.
-	# maybe break this off into another function
-	google = re.compile("^!convert (.*)").search(message)
-	# generate search URL
-	search_page = urllib.urlopen("http://www.google.com/search?hl=en&q="+ re.sub(" ", "+", google.group(1)) +"&btnG=Google+Search")
-	search_page = search_page.readlines()
-	for each in search_page:
-	    # wade through googles mass of html to pull out the conversion
-	    if re.compile("<font size\=\+1><b>(.*?)<\/b>", re.M).search(each):
-		try:
-		    response = re.compile("<font size\=\+1><b>(.*?)<\/b>", re.M).search(each).group(1)
-		    response = re.sub('<sup>', '^', response)
-		    response = re.sub('<.*?>', '', response)
-		    response = re.sub('([0-9]) ([0-9])', '\\1,\\2', response)
-		    entities = re.findall('\&#(.*?)\;', response)
-		    for n in entities:
-			response = re.sub('&#'+n+';', chr(int(n)), response)
-		    self.say(respond, "Conversion | " + response )
-		    return
-		except NameError:
-		    self.say(respond, "Look it up yourself, Google sucks.")
-		    return
