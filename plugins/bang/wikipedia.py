c07ce8681d5b6f365c0f043c6ab2193a2092a47d
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-02-04 08:00:50 +0000

    Update bang.wikipedia to use the newer bang plugin architecture, and reduce code duplication by calling the google plugin.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@202 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/wikipedia.py
+++ plugins/bang/wikipedia.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python
-"""Fishbot Wikipedia Lookup"""
+"""!wikipedia - Search wikipedia"""
 import urllib,re
+import plugins.bang.google
 
-# Spoof Firefox for the purposes of querying
-#urllib.URLopener.version = """Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.8) Gecko/20050609 Firefox/1.0.4"""
-
-def handle_say(self, source, to, message):
-    # self here is going to be the main Fishbot object
-    respond = self.respond_to(source, to)
-
-    if (re.search("^!wikipedia (.*)", message)):
-	# probably shouldn't be considered a simple command.
-	# maybe break this off into another function
-	google = re.search("^!wikipedia (.*)",message)
-	# generate search URL
-	search_page = urllib.urlopen("http://www.google.com/search?hl=en&q="+ re.sub(" ", "+", google.group(1)) +"+site:en.wikipedia.org&btnG=Google+Search")
-	search_page = search_page.readlines()
-	for each in search_page:
-	    # wade through googles mass of html to pull out the first title
-	    # and url.
-	    if re.compile("<\!--m-->(.*?)<\/a>", re.M).search(each):
-		try:
-		    self.say(respond, "Wikipedia - '" + google.group(1) + "'| " + re.sub("<.*?>","",re.compile("<\!--m-->(.*?)<\/a>", re.M).search(each).group(1)) + " [ " + re.search("<\!--m-->.*?<a.*?href=\"(.*?)\">", each, re.M).group(1) + " ]")
-		    return
-		except:
-		    self.say(respond, "Look it up yourself, Google sucks.")
-		    return
+def bang(pipein, arguments, event):
+	search = pipein or arguments
+	return plugins.bang.google.bang("site:en.wikipedia.org " + search, "", event)
