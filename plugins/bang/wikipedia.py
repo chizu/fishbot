00630823d48c061bdc96826e1b247ce0afe6dde2
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-02-04 08:11:42 +0000

    Try to fix bang.wikipedia again.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@205 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/wikipedia.py
+++ plugins/bang/wikipedia.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 """!wikipedia - Search wikipedia"""
 import re,importer
-__import__ = importer.__import__
-import plugins.bang.google
+google = importer.__import__("google", globals(), locals(), "plugins/bang")
 
 def bang(pipein, arguments, event):
 	search = pipein or arguments
-	return plugins.bang.google.bang("site:en.wikipedia.org " + search, "", event)
+	return google.bang("site:en.wikipedia.org " + search, "", event)
