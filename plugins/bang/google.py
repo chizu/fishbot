85ee2b1f73bda35b135f80fa8668c2dfdcf0f671
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-02-04 07:53:43 +0000

    New regular expression for parsing google search results.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@201 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/google.py
+++ plugins/bang/google.py
@@ -2,11 +2,10 @@
 """!google - Fishbot google search"""
 import urllib, re, fishapi
 
-# Spoof Firefox for the purposes of googling.
-#urllib.URLopener.version = """Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.8) Gecko/20050609 Firefox/1.0.4"""
 def bang(pipein, arguments, event):
-    search = pipein or arguments
-    url = "http://www.google.com/search?hl=en&q="+ re.sub(" ", "+", search) +"&btnG=Google+Search"
-    groups = fishapi.http_grep(url, "<\!--m-->.*?<a.*?href=\"(.*?)\">(.*?)</a>")
-    return ("Google - '%s' | %s [ %s ]" % (search, re.sub("<.*?>", "", groups[1]), groups[0]), None)
+	search = pipein or arguments
+	url = "http://www.google.com/search?hl=en&q="+ re.sub(" ", "+", search) +"&btnG=Google+Search"
+	groups = fishapi.http_grep(url, """<!--m-->.*?href="(.*?)".*?>(.*?)</a>(.*?)<span""")
+	print groups
+	return ("Google - '%s' | %s | %s [ %s ]" % (search, re.sub("<.*?>", "", groups[1]),	re.sub("<.*?>", "", groups[2]), groups[0]), None)
 
