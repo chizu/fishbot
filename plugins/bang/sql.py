91adf49c443fefb330d2a594ab503ddc83aa679a
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-02-19 02:00:31 +0000

    bang.sql indentation fixed.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@224 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/sql.py
+++ plugins/bang/sql.py
@@ -4,21 +4,21 @@ import psycopg2
 con = psycopg2.connect(user='fishbotread', host='localhost', database='fishbot')
 
 def bang(pipein, arguments, event):
-    query = arguments or pipein
+	query = arguments or pipein
 	cur = con.cur()
-    try:
+	try:
 		cur.execute(query)
 		cur.connection.commit()
-        results = cursor.fetchall()
+		results = cursor.fetchall()
 		cur.close()
-        if len(results) > 6:
-            return ("Query returned %s rows." % len(results), None)
-        else:
-            values = []
-            for each in results:
-                values.append(str(each))
-            return (values, None)
-    except:
+		if len(results) > 6:
+			return ("Query returned %s rows." % len(results), None)
+		else:
+			values = []
+			for each in results:
+				values.append(str(each))
+			return (values, None)
+	except:
 		cur.connection.rollback()
 		cur.close()
 		raise
