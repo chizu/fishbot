446dd8213540181bd3c0bcb10facdb36a6eae4f8
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-03-10 10:52:10 +0000

    New karma system, weighted based on how many people contribute to each karma entry.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@250 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/karma.py
+++ plugins/bang/karma.py
@@ -4,19 +4,35 @@
 import backend, fishapi
 from plugins.karma import Karma
 
+def calckarma(thing):
+	pcount = 0
+	positive = 0
+	ncount = 0
+	negative = 0
+	for each in thing:
+		score = each.score
+		if score < 0:
+			ncount += 1
+			negative += abs(score)
+		elif score > 0:
+			pcount += 1
+			positive += score
+	return (positive * pcount) - (negative * ncount)
+
 def bang(pipein, arguments, event):
-    token = arguments.strip().lower()
-    if not token:
-        token = fishapi.getnick(event.source).strip().lower()
-        arguments = token
-    if token == 'stats':
-        all_karma = [(x.score, x.string) for x in Karma(-1)]
-        large = max(all_karma)
-        small = min(all_karma)
-        return ("Highest score: %s (%s) - Lowest score: %s (%s)" % (large[0], large[1], small[0], small[1]), None)
-    else:
-        thing = Karma(-1, string=token)
-        if thing:
-            return ("'%s' has a score of: %s" % (arguments, thing.score), None)
-        else:
-            return ("'%s' has neutral karma." % (arguments), None)
+	token = arguments.strip().lower()
+	if not token:
+		token = fishapi.getnick(event.source).strip().lower()
+		arguments = token
+	if token == 'stats':
+		all_karma = [(x.score, x.string) for x in Karma(-1)]
+		large = max(all_karma)
+		small = min(all_karma)
+		return ("Highest score: %s (%s) - Lowest score: %s (%s)" % (large[0], large[1], small[0], small[1]), None)
+	else:
+		thing = Karma(-1, string=token)
+		score = calckarma(thing)
+		if score:
+			return ("'%s' has a score of: %s" % (arguments, score), None)
+		else:
+			return ("'%s' has neutral karma." % (arguments), None)
--- plugins/karma.py
+++ plugins/karma.py
@@ -3,26 +3,23 @@
 import backend
 
 class Karma(backend.DatabaseObject):
-    """Class for tracking karma."""
-    string = str()
-    score = 0
+	"""Class for tracking karma."""
+	string = str()
+	score = 0
+	nick = str()
 
 def karma(self, event):
-    import re, fishapi
-    before, string, operator = re.search(expression[0], event.arguments).groups()
-    string = string.strip().lower()
-    count = 0
-    for each in backend.last(event.source,25):
-        if string + operator in each[4]:
-            count += 1
-    if count > 5:
-        Karma(string=fishapi.getnick(event.source)).score -= 50
-        return
-    if operator == '++':
-        Karma(string=string).score += 1
-    elif operator == '--':
-        Karma(string=string).score -= 1
-    else:
-        print "karma.py: Something odd has happened."
+	import re, fishapi
+	before, string, operator = re.search(expression[0], event.arguments).groups()
+	string = string.strip().lower()
+	item = Karma(string=string, nick=event.source)
+	if operator == '++':
+		if item.score < 5:
+			item.score += 1
+	elif operator == '--':
+		if item.score > -5:
+			Karma(string=string).score -= 1
+	else:
+		print "karma.py: Something odd has happened."
 
 expression = ("(^.*[^\w' ]|^)(.*?)([+]{2}|[-]{2})", karma)
