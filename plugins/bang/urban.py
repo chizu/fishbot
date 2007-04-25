1f00698504d601fb2272bd05a98601ea9403c070
pountaar <pountaar@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-04-25 20:14:50 +0000

    Urban Dictionary search tool.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@253 06b493c4-6b0d-0410-8f1d-df2a14a3f893

---
+++ plugins/bang/urban.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""Look up a word in the Urban Dictionary.
+!urban word"""
+
+def bang(pipein, arguments, event):
+    import SOAPpy
+
+    server = SOAPpy.SOAPProxy("http://api.urbandictionary.com/soap")
+    urban = server.lookup("7df8eccad5391dbbb45e81fa77f4c1a8", arguments)
+
+    if len(urban) > 1:
+        if(len(urban[0].definition) < 256):
+            return (arguments + ": " + urban[0].definition)
+        else:
+            return (urban[0].url)
+
+    return ("Could not find a definition for " + arguments)
+    
