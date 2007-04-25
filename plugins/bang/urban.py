22078eea30c4bb43bcb09893857c473931a86d3d
pountaar <pountaar@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-04-25 20:22:59 +0000

    Removed limit on the length of results.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@255 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/urban.py
+++ plugins/bang/urban.py
@@ -9,10 +9,7 @@ def bang(pipein, arguments, event):
     urban = server.lookup("7df8eccad5391dbbb45e81fa77f4c1a8", arguments)
 
     if len(urban) > 1:
-        if(len(urban[0].definition) < 256):
-            return (arguments + ": " + urban[0].definition + "\n", None)
-        else:
-            return (urban[0].url, None)
+        return (arguments + ": " + urban[0].definition + "\n", None)
 
     return ("Could not find a definition for " + arguments + "\n", None)
     
