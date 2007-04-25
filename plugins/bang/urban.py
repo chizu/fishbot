4e1edd895db94133fa267d9e161f4b77cce8f644
pountaar <pountaar@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-04-25 20:32:00 +0000

    Multi-line fix.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@256 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/urban.py
+++ plugins/bang/urban.py
@@ -9,7 +9,7 @@ def bang(pipein, arguments, event):
     urban = server.lookup("7df8eccad5391dbbb45e81fa77f4c1a8", arguments)
 
     if len(urban) > 1:
-        return (arguments + ": " + urban[0].definition + "\n", None)
+        return (urban[0].definition.replace("\n\n", "\n").split("\n"), None)
 
     return ("Could not find a definition for " + arguments + "\n", None)
     
