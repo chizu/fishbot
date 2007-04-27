92ede53b9a2b6eb86bc9e20712860b26e309ddb2
pountaar <pountaar@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-04-27 09:55:44 +0000

    Added support for selecting a specific record from UD.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@258 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/urban.py
+++ plugins/bang/urban.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
-"""Look up a word in the Urban Dictionary.
-!urban word"""
+"""Look up a word in the Urban Dictionary. Optionally use n'th entry.
+!urban <word> | !urban <n> <word>"""
 
 def bang(pipein, arguments, event):
     import SOAPpy
+    import re
+
+    match = re.compile("^(\d+) (.*)").match(arguments)
+    if not match:
+        num = 0
+    else:
+        (num, arguments) = match.groups()
+
 
     server = SOAPpy.SOAPProxy("http://api.urbandictionary.com/soap")
     urban = server.lookup("7df8eccad5391dbbb45e81fa77f4c1a8", arguments)
 
     if len(urban) > 1:
-        return (urban[0].definition.replace("\n\n", "\n").split("\n"), None)
+        return (urban[num].definition.replace("\n\n", "\n").split("\n"), None)
 
     return ("Could not find a definition for " + arguments + "\n", None)
     
