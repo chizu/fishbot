1e7968e4078b85a0417d27559ebf69ea62d45e07
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-05-10 07:13:52 +0000

    Removing weather module (unmaintained, broken)
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@262 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/weather.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-"""!weather - Weather lookup"""
-import urllib, re, fishapi
-
-def bang(pipein, arguments, event):
-    location = pipein or arguments
-    url = "http://rss.wunderground.com/auto/rss_full/%s.xml?units=both" % location
-    groups = fishapi.http_grep(url, "<\!\[CDATA\[(.*?)\]\]>")
-    return (location + ' - ' + re.sub('&#176;', chr(176), groups[0]), None)
