38321ad6d85db7b4b5da222049a07e51856ba710
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-05-13 05:33:39 +0000

    Update whitespace for the current Fishbot coding guidelines.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@265 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/s.py
+++ plugins/bang/s.py
@@ -7,38 +7,38 @@ import re,sys,os,string,difflib
 import fishapi
 
 def bang(pipein, arguments, event):
-    # This regexp is to split correctly on only unescaped / slashes like
-    # 'asjdf/as[ad/c]df/as\/df' needs.
-    # Returns ['asjdf', 'as[ad/c]df', 'as\\/df']
-    unescaped_slash = re.compile(r"(?<!\\)(?<!\[)/(?!.?\])")
-    if len(unescaped_slash.split(arguments)) == 4:
-        # Assumed to start with s/
-        (pattern, repl, options) = unescaped_slash.split(arguments)[1:]
-        # Number of lines to search
-        if re.search('([1-9])', options):
-            lines = int(re.search('([1-9])', options).group(0)) + 1
-        else:
-            lines = 2
-        # Search globally, or just the user executing the command
-        if 'g' in options or 'M' in options:
-            search_domain = fishapi.backend.last(event.target, lines)[1:]
-        else:
-            search_domain = fishapi.backend.last(event.source, lines)[1:]
-            for each in search_domain:
-                each[4] = each[4].replace('\001ACTION', fishapi.getnick(each[1]))
+	# This regexp is to split correctly on only unescaped / slashes like
+	# 'asjdf/as[ad/c]df/as\/df' needs.
+	# Returns ['asjdf', 'as[ad/c]df', 'as\\/df']
+	unescaped_slash = re.compile(r"(?<!\\)(?<!\[)/(?!.?\])")
+	if len(unescaped_slash.split(arguments)) == 4:
+		# Assumed to start with s/
+		(pattern, repl, options) = unescaped_slash.split(arguments)[1:]
+		# Number of lines to search
+		if re.search('([1-9])', options):
+			lines = int(re.search('([1-9])', options).group(0)) + 1
+		else:
+			lines = 2
+		# Search globally, or just the user executing the command
+		if 'g' in options or 'M' in options:
+			search_domain = fishapi.backend.last(event.target, lines)[1:]
+		else:
+			search_domain = fishapi.backend.last(event.source, lines)[1:]
+			for each in search_domain:
+				each[4] = each[4].replace('\001ACTION', fishapi.getnick(each[1]))
 				each[4] = each[4].replace('\001', '')
-        try:
-            option_values = {'I':re.I, 'L':re.L, 'M':re.M, 'S':re.S, 'U':re.U, 'X':re.X,
-                             'i':re.I,           'g':re.M}
-            # eval, be careful changing this as to not allow arbitrary
-            # python to be executed
-            compiled = re.compile(pattern, options and eval('|'.join(options), None, option_values) or 0)
-            returned = []
-            for each in search_domain:
-                if compiled.search(each[4]):
-                    returned.append("%s meant to say: %s" % (fishapi.getnick(each[1]), compiled.sub(repl, each[4]).replace("\/", "/")))
-            return (returned, None)
-        except:
-            raise
-    else:
-        return ("Invalid regular expression (check for missing /'s)", None)
+		try:
+			option_values = {'I':re.I, 'L':re.L, 'M':re.M, 'S':re.S, 'U':re.U, 'X':re.X,
+							 'i':re.I,			 'g':re.M}
+			# eval, be careful changing this as to not allow arbitrary
+			# python to be executed
+			compiled = re.compile(pattern, options and eval('|'.join(options), None, option_values) or 0)
+			returned = []
+			for each in search_domain:
+				if compiled.search(each[4]):
+					returned.append("%s meant to say: %s" % (fishapi.getnick(each[1]), compiled.sub(repl, each[4]).replace("\/", "/")))
+			return (returned, None)
+		except:
+			raise
+	else:
+		return ("Invalid regular expression (check for missing /'s)", None)
