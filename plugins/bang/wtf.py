74840616135192f845f681fce963aa5a8955ebeb
chizu <chizu@06b493c4-6b0d-0410-8f1d-df2a14a3f893>
2007-05-30 09:01:07 +0000

    Fix flood issue with wtf.
    git-svn-id: https://fishbot.spicious.com/svn/fishbot@266 06b493c4-6b0d-0410-8f1d-df2a14a3f893

--- plugins/bang/wtf.py
+++ plugins/bang/wtf.py
@@ -2,8 +2,11 @@
 import re,os
 
 def bang(pipein, arguments, event):
-    respond = []
-    if arguments:
-	for each in os.popen('wtf ' + re.escape(arguments)):
-	    respond.append((each or ("Gee... I don't know what " + arguments + "means...")))
-    return (respond, None)
+	respond = []
+	if arguments:
+		for each in os.popen('wtf ' + re.escape(arguments)):
+			respond.append((each or ("Gee... I don't know what " + arguments + "means...")))
+	# Fix flood where ... returns all acronyms known.
+	if len(respond) > 12:
+		respond = respond[:12]
+	return (respond, None)
