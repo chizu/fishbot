3d2889d1f87aa41b7746743d86bc310614a4ade4
Nell Hardcastle <chizu@hotaru.cor.spicious.com>
2007-09-14 15:26:38 -0700

    Pull Fishbot from git.

--- plugins/bang/update.py
+++ plugins/bang/update.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-"""!update - Update Fishbot to the latest SVN.
+"""!update - Update Fishbot to the latest git.
 Usage: !update"""
 import os
 
 def bang(pipein, arguments, event):
-    for each in os.popen('svn up').readlines():
+    for each in os.popen('git pull').readlines():
         print each
     return (None, 'performs major surgery on itself.')
