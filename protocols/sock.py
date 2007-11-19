3d62b666e09cdf84121a06e2a2300af19f97e47d
Nell Hardcastle <chizu@hotaru.cor.spicious.com>
2007-11-19 00:47:05 -0800

    Raise the timeout for Freenode again.

--- protocols/sock.py
+++ protocols/sock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import socket
 import protocols.generic
-socket.setdefaulttimeout(300.0) # Socket timeout
+socket.setdefaulttimeout(600.0) # Socket timeout
 
 class SocketEvent(protocols.generic.Event):
 	pass
