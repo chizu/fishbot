b58ffc17412556a94b53466a10eb71ec2df42a4a
Nell Hardcastle <nell@spicious.com>
2009-11-11 15:31:48 -0800

    Add a grep command - inspired by Josh Schonstal's version

---
+++ plugins/bang/grep.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""!grep - Search for strings!
+Usage: <text> | !grep <search string>"""
+import re,os,getopt,string
+
+def bang(pipein, arguments, event):
+    command = 'grep '+ re.escape(arguments) + \
+			  ' <<EOF\n' + pipein.replace('EOF', 'No cheating.') + \
+			  '\nEOF'
+    print command
+    return (os.popen(command).readlines(), None)
