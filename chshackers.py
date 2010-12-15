2636866efc2455c6ef581990c98cf13af2264037
Nell Hardcastle <chizu@spicious.com>
2010-12-15 14:38:37 -0800

    Fix the example script and add a README.

--- chshackers.py
+++ chshackers.py
@@ -6,9 +6,8 @@ import protocols.irc
 def oper(server, username, password):
 	server.oper(username, password)
 
-bot = Fishbot({"chshackers":protocols.irc.Client(nick="Fishbot", realname="Fishbot", hostname="grandpa.chshackers.com", port=6667)})
-# Oper on chshackers
+bot = Fishbot({"chshackers":protocols.irc.Client(nick="Fishbot", realname="Fishbot", hostname="irc.chshackers.com", port=6667)})
+# Oper example
 bot.servers["chshackers"].triggers.register("001", oper, (bot.servers["chshackers"], "username", "password"))
 bot.servers["chshackers"].join("#chshackers")
-bot.servers["chshackers"].join("#mmo-dev")
 bot.start()
