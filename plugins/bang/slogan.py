eb11700fe3413954154dd492c72dbe009478b9b0
Nell Hardcastle <chizu@spicious.com>
2012-01-11 04:45:37 +0000

    Fix !slogan
    Drop usage of the http_grep function to make a POST request. Might want to revisit that.

--- plugins/bang/slogan.py
+++ plugins/bang/slogan.py
@@ -3,7 +3,8 @@ import fishapi,re,urllib
 
 def bang(pipein, arguments, event):
     subject = pipein or arguments
-    url = "http://www.sloganizer.net/en/?%s" % urllib.urlencode({'slogan':subject})
-    groups = fishapi.http_grep(url, '<p class="slogan">.<b>(.*)</b>.</p>')
-    print groups
-    return (groups[0], None)
+    url = "http://www.sloganizer.net/en/ajax.php"
+    data = urllib.urlencode({"slogan":subject, "id":"1326238012-b2cea363a282c4517bdef74a98a964da"})
+    f = urllib.urlopen(url, data)
+    slogan = f.read().split('<')[1].split('>')[1]
+    return (slogan, None)
