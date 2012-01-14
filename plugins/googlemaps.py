5fc98b97c1a87a43c9e5956ff1edcc25dbd3cd7f
Nell Hardcastle <chizu@spicious.com>
2012-01-14 21:25:49 +0000

    Remove some broken or insecure plugins.

--- plugins/googlemaps.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-def cowsgomoo(self, event):
-    event.server.say(self.respond_to(event.source, event.target), "<toshi>GoOgLe MaPs!?!?</toshi>")
-
-expression = ("(?=(.*google.*))(?=(.*map.*))", cowsgomoo)
