1dcef81f28926656a88c5b58ff885ca3e7c60926
Nell Hardcastle <chizu@spicious.com>
2012-01-18 02:29:16 +0000

    Add an identify example (probably more useful on more networks than oper).

--- example.py
+++ example.py
@@ -3,11 +3,15 @@
 from fishbot import Fishbot
 import protocols.irc
 
+def identify(server, password):
+    server.message("nickserv", "identify {0}".format(password))
+
 def oper(server, username, password):
-	server.oper(username, password)
+    server.oper(username, password)
 
 bot = Fishbot({"example":protocols.irc.Client(nick="Fishbot", realname="Fishbot", hostname="irc.example.com", port=6667)})
 # Oper example
 bot.servers["example"].triggers.register("001", oper, (bot.servers["example"], "username", "password"))
+bot.servers["example"].triggers.register("001", identify, (bot.servers["example"], "ident-password"))
 bot.servers["example"].join("#example")
 bot.start()
